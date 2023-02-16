0468ae9f8fdc50791b5a1981556f443368cc2a4a
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-16 14:06:08 -0800

    C program that prints

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - program that prints line to tyhe standard error
- * Return: 1  (means successful)
+ * Return: 1
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
