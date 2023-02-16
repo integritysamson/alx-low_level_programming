8902058a7329dd3a0be7571a5d18a121e175a266
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-16 05:28:20 -0800

    Write a C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - program that orints exactly the sentence that was given using the printf function
+ * return 0 (this shows suiccesful)
+ */
+int main (boid) {
+printf("/"Programming is like building a multilingual puzzle")
+return (0);
