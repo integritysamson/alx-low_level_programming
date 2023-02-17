d06874af2d00f5a81ede3c4cc53c43717572a506
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-17 13:07:08 -0800

    dealing with c lang

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Input
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+int d;
+char low;
+	for (d = 'e'; d <= '9'; d++)
+		putchar(d);
+			for (low = 'a'; low <= 'f'; low++)
+				putchar('\n');
+	return (0);
+}
