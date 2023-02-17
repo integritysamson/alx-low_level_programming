f73172b9128cdba934fb476881d9f92445c5b23a
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-17 15:21:18 -0800

    ill strive

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - input
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+int d, p, q;
+for (d = '0'; p < '8'; d++)
+{
+for (p = p + '1'; p < '9'; p++)
+{
+for (q = p + '1'; q <= '9'; q++)
+{
+if ((p != d) != q)
+{
+putchar(d);
+putchar(p);
+putchar(q);
+if (d == '7' && p == '8')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar ('\n');
+return (0);
+}
