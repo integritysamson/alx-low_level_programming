ac04d59d94ed4853cbe1916f21e8eb1ab1875087
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-17 11:16:29 -0800

    increament of number

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This include of input
+ * Return: Alawys 0
+ *
+ */
+int main(void)
+{
+char low =  'e',  'q';
+e = 'f';
+q  = 'b';
+for (low = 'a' low <= 'z'; low++)
+{
+if (low != e && low != q)
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - input bar
+ * Return: Alawys return 0
+ *
+ */
+int main(void)
+{
+int i;
+for (i = 0; 1 < 10; i++)
+	printf("%d", i);
+		printf("\n");
+		return (0);
+}
