96f6716a5731a9a3917a6fde494b002c0aefe308
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-17 13:32:42 -0800

    rite a program that prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This the input area
+ * Return: Always ensure 0 is returned
+ *
+ */
+int main(void)
+{
+int d;
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
