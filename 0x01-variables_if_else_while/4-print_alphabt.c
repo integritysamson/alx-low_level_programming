9440872412fca0250e52c566c3d5c03ccfc8a328
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-17 16:36:19 -0800

    you

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-char low =  'e',  'q';
-e = 'f';
-q  = 'b';
-for (low = 'a' low <= 'z'; low++)
+char low = 'a';
+for (low = 'a'; low <= 'z'; low++)
+if (low != 'q' && low != 'e')
 {
-if (low != e && low != q)
+putchar(low);
 }
 putchar('\n');
 return (0);
