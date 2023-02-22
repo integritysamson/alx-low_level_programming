2ea17c93a45a54cc75617df758c8edf9335cfeea
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-22 05:40:15 -0800

    a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,11 +33,12 @@ unsigned long half1, half2;
 	printf("%lu%lu", half1, half2);
 	if (count != 98)
 	printf(", ");
-	}
 	fib1_half1 = fib2_half1;
 	fib1_half2 = fib2_half2;
 	fib2_half1 = half1;
 	fib2_half2 = half2;
+	}
 	printf("\n");
 	return (0);
 }
+
