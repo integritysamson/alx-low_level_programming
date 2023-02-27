a5ce355e5833a0095de8d114db5b059878ad878d
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-02-27 04:32:36 -0800

    function that prints n elements of an array of integers, followed by a new line.

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -7,9 +7,8 @@
  */
 int main(void)
 {
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+char *str;
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+puts(str);
+return (0);
 }
