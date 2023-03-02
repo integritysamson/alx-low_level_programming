fa6912660b935c7acd8c986dd8cda3230519a39d
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-03-02 13:46:10 -0800

    correction

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+int _putchar(void c);
 void print_number(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
