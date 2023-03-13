24e40d6814c0b2d959139c4d060e102c4257bf40
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-03-13 00:15:10 -0700

    push

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,5 +12,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return putchar(c); 
 }
