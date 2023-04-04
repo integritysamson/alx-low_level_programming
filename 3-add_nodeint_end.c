503d6660ac615a303ba115944333aebf5df55fd7
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-04-04 05:39:56 -1100

    function that a add a new node at the end of listint.

--- 3-add_nodeint_end.c
+++ 3-add_nodeint_end.c
@@ -5,7 +5,6 @@
  * of a linked list
  * @head: header file
  * @n: n element
- *
  * Return: address of the new element. NUll if it failed.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
