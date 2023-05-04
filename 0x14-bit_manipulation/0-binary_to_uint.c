3f378130f5a9d41aa2790590587e5af400f75290
OJO SAMSON OLUWATOPE <integritysamson@gmail.com>
2023-05-04 06:14:06 -1200

    The bit manipulation project

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,22 @@
 #include "main.h"
 /**
- * binary_to_uint - This convert the binary number to an unsigned interger
+ * binary_to_uint - This convert the binary number to 
+ * an unsigned interger
  * @b: The binary number
  * return: to return unsigned interger 
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int a;
-	int lenght, base_2;
-	if (!b)
-		return (0);
-	a = 0;
-	for (lenght = 0; b[lenght] != '\0'; lenght++)
-		;
-	for(lenght--, base_2 = 1; lenght >= 0; lenght--, base_2 *= 2)
-	{
-		if (b[lenght] != '0' && b[lenght] != '1')
-		{
-			return (0);
-		}
-		if (b[lenght] & 1)
-		{
-			a += base_2;
-		}
-	}
-	return (a);
+int a;
+unsigned int base_two = 0;
+if (!b)
+return (0);
+for (a = 0; b[a] != '\0'; a++)
+{
+if (b[a] < '0' || b[a] > '1')
+return (0);
+base_two = 2 * base_two + (b[a] - '0');
+}
+return (base_two);
 }
 
